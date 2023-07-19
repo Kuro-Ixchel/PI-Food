1c69266f3081e6a3929875ee9dce2ee620689e52
Kuro-Ixchel <99032282+Kuro-Ixchel@users.noreply.github.com>
2023-07-19 11:20:51 -0600

    sin front

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,9 @@
-import axios from "axios";
-import Form from "./components/Form/Form";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import NavBar from "./components/NavBar/NavBar";
-import { Detail } from "./components/Detail/Detail";
-import Landing from "./components/LandingPage/Landing";
-
-import { useState } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
-
-axios.defaults.baseURL = "http://localhost:3000/";
+import './App.css';
 
 function App() {
-  const { pathname } = useLocation();
-  const [paginaActual, setPaginaActual] = useState(1);
   return (
-    <div>
-      {pathname !== "/" && (
-        <NavBar paginaActuale={paginaActual} setPaginaActual={setPaginaActual} />
-      )}
-      <Routes>
-        <Route path="/" element={<Landing/>} />
-        <Route
-          path="/home"
-          element={
-            <Home paginaActual={paginaActual} setPaginaActual={setPaginaActual} />
-          }
-        />
-        <Route path="/about" element={<About/>}/>
-        <Route path="/detail/:id" element={<Detail/>}/>
-        <Route path="/form" element={<Form/>}/>
-      </Routes>
-      <Footer></Footer>
+    <div className="App">
+      <h1>Henry Food</h1>
     </div>
   );
 }
